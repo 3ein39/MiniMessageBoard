2aac8df514b634df84f2e8325162f058e28724e0
HusseinHany <hhany3997@gmail.com>
2023-09-27 15:12:11 +0300

    Add some messages

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
+
+// some placeholder messages
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    }
+    , {
+          text: "Hello World!",
+          user : "Charles",
+          added: new Date()
+    }
+    , {
+          text: "Meow!",
+          user : "Sayed the cat",
+          added: new Date()
+  }]
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
